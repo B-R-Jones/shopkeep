896b0b3aa8fccc3703b5b559e33fb551f7a86638
Brent Jones <brentjones199@gmail.com>
2019-08-26 18:52:31 -0500

    basic armor view created

--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_162618) do
+ActiveRecord::Schema.define(version: 2019_08_26_213055) do
 
   create_table "armors", force: :cascade do |t|
-    t.string "base_armor"
-    t.string "category"
-    t.integer "ac_bonus"
-    t.integer "shield_bonus"
-    t.integer "max_dex_bonus"
-    t.integer "armor_check_penalty"
-    t.string "speed"
-    t.integer "weight"
+    t.string "base_armor", null: false
+    t.string "category", null: false
+    t.integer "ac_bonus", null: false, default: 0
+    t.integer "shield_bonus", null: false, default: 0
+    t.integer "max_dex_bonus", null: false, default: 0
+    t.integer "armor_check_penalty", null: false, default: 0
+    t.string "speed", null: false, default: 0
+    t.integer "weight", null: false, default: 0
     t.string "special"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "arcane_spell_failure"
+    t.integer "arcane_spell_failure", null: false, default: 0
   end
 
   create_table "weapons", force: :cascade do |t|
-    t.string "base_weapon"
-    t.string "category"
-    t.integer "dmg_s"
-    t.integer "dmg_m"
-    t.integer "critical"
-    t.string "critical_range"
-    t.integer "range"
-    t.integer "weight"
+    t.string "base_weapon", null: false
+    t.string "category", null: false
+    t.integer "dmg_s", null: false
+    t.integer "dmg_m", null: false
+    t.integer "critical", null: false, default: 0
+    t.string "critical_range", null: false
+    t.integer "range", null: false, default: 5
+    t.integer "weight", null: false, default: 0
     t.string "special"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "hands"
-    t.string "style"
-    t.string "damage_type"
+    t.integer "hands", null: false, default: 1
+    t.string "style", null: false
+    t.string "damage_type", null: false
   end
 
 end
