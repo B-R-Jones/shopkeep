8819cb0fc3a38e1efcbf688f1c983d27b0be394c
Brent Jones <brentjones199@gmail.com>
2019-08-26 15:58:38 -0500

    remove errant merge conflict markers from seed.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :weapons
-<<<<<<< HEAD
   resources :armors
-=======
->>>>>>> 3e3f2c264d27d90c313c736891b1ca498e7b9bdc
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
