6be6e74803a0b6c39df9027e6a8f14231fd769c1
Brent Jones <brent.r.jones@uscis.dhs.gov>
2019-09-04 19:08:46 +0000

    more work on user and money model associations and methods

--- app/models/money.rb
+++ app/models/money.rb
@@ -1,28 +1,3 @@
 class Money < ApplicationRecord
   belongs_to :user
-
-  #attr_accessor :platinum
-
-  define_attribute_methods :platinum
-
-  # def self.initialize
-  #   @platinum = platinum
-  # end
-
-  def platinum
-    @platinum
-  end
-
-  # def self.gold
-  #   Money.gold
-  # end
-
-  # def self.silver
-  #   Money.silver
-  # end
-
-  # def self.copper
-  #   Money.copper
-  # end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,12 @@ class User < ApplicationRecord
   has_one :inventory
   has_one :money
 
-  def platinum
-    Money.get_platinum
+  def wallet
+    pl = Money.find(self.id).platinum.to_s + 'p'
+    go = Money.find(self.id).gold.to_s + 'g'
+    si = Money.find(self.id).silver.to_s + 's'
+    co = Money.find(self.id).copper.to_s + 'c'
+    @wallet = "Wallet: #{pl}, #{go}, #{si}, #{co}"
   end
 
 end
